05c74c3fea6c58b2ff4e76204d1af2af0033b306
Dray <riccardopaulucci@gmail.com>
2026-05-17 09:39:48 +0200

    Fix CORS middleware initialization order

--- backend/main.py
+++ backend/main.py
@@ -14,17 +14,6 @@ import json
 # ----------------------------------------------------------------
 
 
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  #  React dev server url
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("SERVER STARTING")
@@ -40,7 +29,16 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ----------------------------------------------------------------
 # DATA DEFINITIONS
